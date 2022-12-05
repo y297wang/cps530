5c7beb6bb9696483c26c8cc607b1c741e2b738cb
Jessica Wang <yu4.wang@ryerson.ca>
2022-12-05 03:22:59 -0500

    deploy app to github pages

---
+++ pages/index.js
@@ -0,0 +1,138 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+        <title>Problem 3: Frameworks</title>
+        </Head>
+
+        <header className={styles.Title}>Next.js</header>
+        <br></br>
+        <header className={styles.Name}>Jessica Wang</header>
+        <header className={styles.Info}>ID: 500586571</header>
+
+        <br></br><br></br>
+
+        <main>
+        <div className={styles.Main}>
+            <header>Problem 3</header>
+
+            <p>
+                The framework used to create this web page is Next.js.
+                Originally I wanted to use React, but I learned that React is a library and not a framework so I decided to use Next.js instead because it uses React applications.
+            </p>
+
+            <p className={styles.Info2}>Installing Next.js</p>
+
+            <p>
+                I installed Next.js by first installing Node.js and Git for Windows.
+                Then, I used the Git terminal to bootstrap Vercel's example blog Next.js app to my computer.
+                After that, I was able to run the run dev command and see the web pages on my localhost:3000 address.
+            </p>
+
+            <p className={styles.Info2}>Building the Web Page</p>
+
+            <p>
+                During the course of this semester, I was able to create the HTML and CSS template I use for every lab.
+                For each lab I usually change the colour palette and the content of the web page.
+                I was able to import my styles into React.js using both the style tag on the page and the imported css file from the styles directory in the project.
+                For the titles, headings, and layout I was able to learn the JavaScript used to add styles to the elements.
+                Everything else was basically the same as normal HTML.
+            </p>
+
+            <p className={styles.Info2}>Difficulties</p>
+
+            <p>
+                wip
+            </p>
+
+            <hr></hr>
+
+            <p>
+                ♦ <a href="https://www.cs.ryerson.ca/~y297wang/lab10.html">Home</a> ♠
+            </p>
+        </div>
+        </main>
+
+        <br></br><br></br><br></br><br></br>
+        
+        <footer>
+            <p>Jessica Wang 2022<br></br>
+            CPS530 Lab 10c</p>
+        </footer>
+
+      <style jsx>{`
+        
+      `}</style>
+
+      <style jsx global>{`
+        body {
+            background-color: #FAE3D9;
+            padding: 2px;
+            font-size: 14px;
+            font-family: Tahoma, Verdana;
+            text-align: center;
+            color: #000000;
+        }
+        
+        a:link, a:visited {
+            color: #61C0BF;
+            text-decoration: none;
+        }
+        
+        a:hover {
+            color: #BBDED6;
+            text-decoration: none;
+        }
+        
+        header {
+            font-size: 40px;
+            font-family: 'Times New Roman', Times;
+            line-height: 35px;
+            letter-spacing: 2px;
+            text-align: center;
+            color: #000000;
+            border-bottom: 1px dashed #FFB6B9;
+        }
+        
+        nav {
+            font-family: 'Courier New', Courier;
+            font-size: 20px;
+            text-align: center;
+        }
+        
+        table {
+            margin-left: auto;
+            margin-right: auto;
+            width: 400px;
+            text-align: center;
+            font-family: Georgia, 'Times New Roman', Times;
+            font-size: 20px;
+        }
+        
+        img {
+            width: 300px;
+        }
+        
+        hr {
+            border-bottom: 1px dashed #FFB6B9;
+            border-top: 0px;
+        }
+        
+        footer {
+            position: relative;
+            text-align: center;
+            font-weight: bold;
+        }
+        
+        @media only screen and (min-width: 320px) and (max-width: 508px) {
+            table {
+                width: 90%;
+            }
+        }
+      `}</style>
+    </div>
+  )
+}
