3120f2cc2e180ad08038d7f9865aa1fe4a2293b1
Jessica Wang <yu4.wang@ryerson.ca>
2022-12-05 04:14:50 -0500

    updated text

--- pages/index.js
+++ pages/index.js
@@ -21,31 +21,39 @@ export default function Home() {
 
             <p>
                 The framework used to create this web page is Next.js.
-                Originally I wanted to use React, but I learned that React is a library and not a framework so I decided to use Next.js instead because it uses React applications.
+                Originally I wanted to use React, but I learned that React is a library and not a framework so I decided to use Next.js instead because it uses React applications and JavaScript.
             </p>
 
             <p className={styles.Info2}>Installing Next.js</p>
 
             <p>
                 I installed Next.js by first installing Node.js and Git for Windows.
-                Then, I used the Git terminal to bootstrap Vercel's example blog Next.js app to my computer.
-                After that, I was able to run the run dev command and see the web pages on my localhost:3000 address.
+                Then, I used the Git terminal to bootstrap Vercel's template Next.js app to my computer.
+                After that, I was able to run the "git run dev" command and see the web page on my localhost:3000 address and edit the pages in my IDE.
             </p>
 
             <p className={styles.Info2}>Building the Web Page</p>
 
             <p>
-                During the course of this semester, I was able to create the HTML and CSS template I use for every lab.
-                For each lab I usually change the colour palette and the content of the web page.
-                I was able to import my styles into React.js using both the style tag on the page and the imported css file from the styles directory in the project.
-                For the titles, headings, and layout I was able to learn the JavaScript used to add styles to the elements.
+                Throughout the semester, I was able to create the HTML and CSS template that I use for every lab.
+                For each lab I usually change the colour palette and the content of the web page to complete the week's tasks.
+                I was able to import my styles into React.js using both the style tag on the page and the imported CSS file from the styles directory in the project.
+                For the titles, headings, and the layout I was able to learn the JavaScript syntax used to add styles to the elements.
                 Everything else was basically the same as normal HTML.
             </p>
 
             <p className={styles.Info2}>Difficulties</p>
 
             <p>
-                wip
+              I had a lot of difficulty getting the web page hosted on GitHub.
+              At first, I had trouble pushing my directory onto GitHub itself.
+              I kept getting errors that the path that I was trying to commit to wasn't found.
+              I watched two YouTube videos, one on how to upload Next.js apps to GitHub and the other on how to upload React apps to GitHub because they are very similar.
+              After following the instructions in both videos, I realized that I was missing the GitHub Pages dependency in my package.json and my branch was set to main instead of master.
+              When those errors were fixed, my directory was uploaded onto GitHub but the GitHub Pages wasn't showing my web page (I got a 404 error).
+              After more Googling, I found out that the settings for GitHub Pages had moved since the videos I watched were created and it needed to be manually activated.
+              I deployed the site, and finally my web page was live.
+
             </p>
 
             <hr></hr>
